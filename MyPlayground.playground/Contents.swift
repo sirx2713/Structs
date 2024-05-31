df75f6b5542237eb4ddca052f699e2303b32c1cb
Mabende <belindamabende@gmail.com>
2024-05-31 15:34:02 +0200

    Initial Commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+struct isad{
+    let name:String = "iSAD"
+    let age:Int = 27
+    
+    func aboutMe(){
+        
+        print("Hello " + name + " you are " + String(age) + " years old!")
+    }
+}
